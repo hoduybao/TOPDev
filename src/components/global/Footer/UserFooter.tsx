0c2d11840daaf85a29b8135dfe3dd10ec697e1da
qhungo0125 <hungngojan25@gmail.com>
2024-03-18 17:48:07 +0700

    add footer

--- src/components/global/Footer/UserFooter.tsx
+++ src/components/global/Footer/UserFooter.tsx
@@ -1,13 +1,78 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { FooterMenu } from '../../../+core/constants/user.constant';
+
+const MenuHeader = ({ title }: { title: string }) => {
+  return <div className='font-bold mb-2'>{title}</div>;
+};
+
+const MenuItem = ({ name, url }: { name: string; url: string }) => {
+  return (
+    <div>
+      <Link to={url}>{name}</Link>
+    </div>
+  );
+};
+
+const Menu = ({ children }: { children: React.ReactNode }) => {
+  return <div className='flex flex-col gap-1'>{children}</div>;
+};
+
+const MainSession = ({ children }: { children: React.ReactNode }) => {
+  return <div className='xl:col-span-4 sm:col-span-12 col-span-12 mb-2'>{children}</div>;
+};
+
+const SubSession = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className='xl:col-span-2 lg:col-span-6 sm:col-span-12 col-span-12 mb-2'>{children}</div>
+  );
+};
 
 const UserFooter = () => {
   return (
-    <div className='flex items-center justify-center w-full'>
-      <img
-        src='https://www.operationgratitude.com/wp-content/uploads/2023/04/starbucks-logo.png'
-        className='w-50 h-20 text-center'
-        alt=''
-      />
+    <div className='w-full px-4 py-16 bg-userFooter flex justify-center items-center'>
+      <div className='grid grid-cols-12 w-full md:max-w-[37.5rem]'>
+        <MainSession>
+          <div className='mb-2'>
+            <img src='/assets/icons/td-logo.png' alt='logo' />
+          </div>
+          <div>{FooterMenu.address}</div>
+          <div>{FooterMenu.contact}</div>
+        </MainSession>
+        <SubSession>
+          <MenuHeader title={FooterMenu.topdevAbout.title} />
+          <Menu>
+            {FooterMenu.topdevAbout.menu.map((item, index) => (
+              <MenuItem key={index} name={item.name} url={item.url} />
+            ))}
+          </Menu>
+        </SubSession>
+
+        <SubSession>
+          <MenuHeader title={FooterMenu.candidate.title} />
+          <Menu>
+            {FooterMenu.candidate.menu.map((item, index) => (
+              <MenuItem key={index} name={item.name} url={item.url} />
+            ))}
+          </Menu>
+        </SubSession>
+
+        <SubSession>
+          <MenuHeader title={FooterMenu.recuitment.title} />
+          <Menu>
+            {FooterMenu.recuitment.menu.map((item, index) => (
+              <MenuItem key={index} name={item.name} url={item.url} />
+            ))}
+          </Menu>
+        </SubSession>
+        <SubSession>
+          <MenuHeader title={FooterMenu.follow.title} />
+          <MenuHeader title={FooterMenu.install.title} />
+        </SubSession>
+
+        <div className='col-span-12 text-center pt-16'>
+          <div>{FooterMenu.copyRight}</div>
+        </div>
+      </div>
     </div>
   );
 };
