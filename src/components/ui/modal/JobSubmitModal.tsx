de04ce36d6d2c3dcd1c2a486ba9a6804a03669b3
qhungo0125 <hungngojan25@gmail.com>
2024-03-19 22:07:47 +0700

    add firebase upload

--- src/components/ui/modal/JobSubmitModal.tsx
+++ src/components/ui/modal/JobSubmitModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Modal, Spin } from 'antd';
+import { Button, Form, Modal, Spin, Upload } from 'antd';
 // import companyData from '../../../draft/company.json';
 // import jobData from '../../../draft/job.json';
 // import formData from '../../../draft/application.json';
@@ -8,15 +8,25 @@ import { useParams } from 'react-router-dom';
 import { useGetJobByIdQuery } from '../../../+core/redux/apis/common/job/job.api';
 import { useGetCompanyByIdQuery } from '../../../+core/redux/apis/common/company/company.api';
 import { CustomTextInput } from '../form/CustomTextInput';
+import firebaseApp from '../../../config/firebase';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 
 const JobSubmitModal = () => {
+  const { jobId, companyId } = useParams<{ jobId: string; companyId: string }>();
+  const { data: jobResponse, isLoading } = useGetJobByIdQuery(jobId);
+  const { data: companyResponse, isLoading: isLoadingCompany } = useGetCompanyByIdQuery(companyId);
+
   const [form] = Form.useForm();
+
+  console.log(firebaseApp);
+
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const showModal = () => {
     setIsModalOpen(true);
   };
-  const handleOk = () => {
+  const handleOk = async () => {
     console.log(form.getFieldsValue());
+
     // setIsModalOpen(false);
   };
   const handleCancel = () => {
@@ -26,11 +36,6 @@ const JobSubmitModal = () => {
   const onSubmitForm = (values: any) => {
     console.log(values);
   };
-
-  const { jobId, companyId } = useParams<{ jobId: string; companyId: string }>();
-  const { data: jobResponse, isLoading } = useGetJobByIdQuery(jobId);
-  const { data: companyResponse, isLoading: isLoadingCompany } = useGetCompanyByIdQuery(companyId);
-
   return (
     <Spin spinning={isLoading || isLoadingCompany}>
       {jobResponse && companyResponse && (
@@ -60,7 +65,66 @@ const JobSubmitModal = () => {
               <CustomTextInput label='Email' name='email' />
               <CustomTextInput label='Số điện thoại' name='phone' />
               <CustomTextInput label='Đoạn giới thiêụ bản thân' name='intro' />
-              <CustomTextInput type='file' label='CV của bạn' name='cv' />
+              <input
+                name='cv'
+                onChange={async (e) => {
+                  if (!e.target.files || !e.target.files[0]) {
+                    return;
+                  }
+                  const file = e.target.files && e.target.files[0];
+                  const handleImageUpload = async () => {
+                    try {
+                      if (!file || !file.name) {
+                        return;
+                      }
+                      const fileName = new Date().getTime() + '-' + file.name;
+
+                      const storage = getStorage(firebaseApp);
+                      const storageRef = ref(storage, `company/${companyId}/cv/${fileName}`);
+                      const uploadTask = uploadBytesResumable(storageRef, file);
+
+                      await new Promise<void>((resolve, reject) => {
+                        uploadTask.on(
+                          'state_changed',
+                          (snapshot) => {
+                            const progress =
+                              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                            console.log('Upload is ' + progress + '% done');
+                            switch (snapshot.state) {
+                              case 'paused':
+                                console.log('Upload is paused');
+                                break;
+                              case 'running':
+                                console.log('Upload is running');
+                                break;
+                            }
+                          },
+                          (err) => {
+                            console.log('err>>>', err);
+                            reject(err);
+                          },
+                          () => {
+                            getDownloadURL(uploadTask.snapshot.ref)
+                              .then((downloadURL) => {
+                                form.setFieldValue('cvUrl', downloadURL);
+                                resolve();
+                              })
+                              .catch((err) => {
+                                console.log('err>>>', err);
+                                reject(err);
+                              });
+                          },
+                        );
+                      });
+                    } catch (error) {
+                      // setIsLoading(false);
+                    }
+                  };
+                  await handleImageUpload();
+                }}
+                type='file'
+              />
+              <CustomTextInput label='CV Url' name='cvUrl' />
             </Form>
           </Modal>
         </div>
