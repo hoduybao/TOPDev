456f766c385f6c406299ce92b08a24f72bee3e50
minhtrifit <minhtri.fit@gmail.com>
2024-03-19 14:33:58 +0700

    complete admin review hr post UI

---
+++ src/components/global/Admin/Content/PendingTab.tsx
@@ -0,0 +1,103 @@
+import { Table, Space, Button, Input } from 'antd';
+import type { TableColumnsType } from 'antd';
+import type { SearchProps } from 'antd/es/input/Search';
+import { v4 as uuidv4 } from 'uuid';
+
+import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
+
+import { JobType } from '@/+core/utilities/types/recruitment.type';
+import JobMockData from '../../../../draft/job.json';
+
+const { Search } = Input;
+
+interface DataType extends JobType {
+  key: React.Key;
+}
+
+const columns: TableColumnsType<DataType> = [
+  {
+    title: 'ID',
+    dataIndex: 'id',
+  },
+  {
+    title: 'Title',
+    dataIndex: 'title',
+    render: (text: string) => <p>{text}</p>,
+  },
+  {
+    title: 'Company ID',
+    dataIndex: 'companyId',
+  },
+  {
+    title: 'Job type',
+    dataIndex: 'type',
+  },
+  {
+    title: 'Contact type',
+    dataIndex: 'typeContract',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (_, record) => (
+      <Space size='middle'>
+        <Button icon={<CheckOutlined />}>Approve {record.id}</Button>
+        <Button type='primary' icon={<CloseOutlined />} danger>
+          Ignore
+        </Button>
+      </Space>
+    ),
+  },
+];
+
+const PendingTab = () => {
+  const data: DataType[] = [
+    {
+      key: uuidv4(),
+      ...JobMockData,
+    },
+  ];
+
+  const rowSelection = {
+    onChange: (selectedRowKeys: React.Key[], selectedRows: DataType[]) => {
+      console.log(`Selected keys: ${selectedRowKeys}`);
+      console.log('Selected rows: ', selectedRows);
+    },
+  };
+
+  const onSearch: SearchProps['onSearch'] = (value, _e, info) => {
+    console.log(info?.source, value);
+  };
+
+  return (
+    <div className='flex flex-col items-center'>
+      <div className='w-full py-4 flex gap-3 flex-wrap items-center justify-between'>
+        <Space size='small'>
+          <Button>Approve all</Button>
+          <Button type='primary' danger>
+            Reject all
+          </Button>
+        </Space>
+        <div>
+          <Search
+            className='w-[300px] md:w-[400px]'
+            placeholder='Search...'
+            onSearch={onSearch}
+            allowClear
+          />
+        </div>
+      </div>
+      <div className='w-full'>
+        <Table
+          rowSelection={{
+            ...rowSelection,
+          }}
+          columns={columns}
+          dataSource={data}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PendingTab;
