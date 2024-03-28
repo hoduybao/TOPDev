d455b4f69b0272ba39de6c98cafe19999c817487
20120077 <ngqghien@gmail.com>
2024-03-28 08:12:46 +0700

    [update] Add more mock data

--- src/components/global/layout/Layout.tsx
+++ src/components/global/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Layout as LayoutAntDesign } from 'antd';
 import { Outlet } from 'react-router-dom';
-import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 const { Content } = LayoutAntDesign;
@@ -9,12 +8,11 @@ export default function Layout() {
   return (
     <LayoutAntDesign className='bg-white'>
       <Header />
-      <LayoutAntDesign className={`bg-white duration-500 ease-in-out ' `}>
+      <LayoutAntDesign className={`bg-white duration-500 ease-in-out`}>
         <Content className='m-auto transition w-full'>
           <Outlet />
         </Content>
       </LayoutAntDesign>
-      <Footer />
     </LayoutAntDesign>
   );
 }
