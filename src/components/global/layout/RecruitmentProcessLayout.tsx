ac936afa023a2de6fa2fdea962fbc5043ced1338
minhtrifit <minhtri.fit@gmail.com>
2024-03-29 13:53:03 +0700

    update hr job management paging

--- src/components/global/layout/RecruitmentProcessLayout.tsx
+++ src/components/global/layout/RecruitmentProcessLayout.tsx
@@ -9,7 +9,7 @@ export default function RecruitmentProcessLayout() {
     <LayoutAntDesign className='bg-white'>
       <Header />
       <LayoutAntDesign
-        className={`bg-primary-white mt-[46px] h-[calc(100vh-46px)] duration-500 ease-in-out overflow-x-auto`}
+        className={`bg-primary-white mt-[46px] min-h-[calc(100vh-46px)] duration-500 ease-in-out overflow-x-auto`}
       >
         <Content className='m-auto transition w-full'>
           <Outlet />
--- src/routes/route.constant.ts
+++ src/routes/route.constant.ts
@@ -1,8 +1,8 @@
 export const MY_ROUTE = {
   HOME: '',
   RECRUITMENT: '/recruitment',
-  RECRUITMENT_PROCESS: '/recruitment/process',
-  RECRUITMENT_DETAIL: '/recruitment/process/detail-application/:id',
+  RECRUITMENT_PROCESS: '/recruitment/:jobId',
+  RECRUITMENT_DETAIL: '/recruitment/:jobId/:applicationId',
   ADMIN: '/admin',
   JOB: 'jobs/:companyId/:jobId',
 };
