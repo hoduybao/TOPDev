5695dfa991317f92d3962e780664c12d1e2a0902
trungnguyen <hotrungnguyen168@gmail.com>
2024-04-03 08:13:07 +0700

    Login by github and google for candidate

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -74,10 +74,84 @@ const LoginPage = () => {
             </ul>
             {activeTab == '1' && (
               <div className='p-8'>
-                <button className='p-4 rounded border-solid border-1 border-black'> hello</button>
+                <button
+                  className='p-4 mb-3 rounded w-full flex justify-center font-bold hover:shadow-lg hover:shadow-slate-500/20'
+                  style={{
+                    border: `solid 1px ${colors.black[300]}`,
+                    color: `solid 1px ${colors.black[300]}`,
+                  }}
+                >
+                  <img
+                    src='https://accounts.topdev.vn/asset/images/google.svg'
+                    alt='Google'
+                    className='h-6 mr-3'
+                  />
+                  Tiếp tục với Google
+                  {/* <span className='ml-3 font-bold'></span> */}
+                </button>
+
+                <button
+                  className='p-4 mb-3 rounded w-full flex justify-center font-bold hover:shadow-lg hover:shadow-slate-500/20'
+                  style={{
+                    background: colors.black[300],
+                    color: 'white',
+                  }}
+                >
+                  <img
+                    src='https://accounts.topdev.vn/asset/images/github.svg'
+                    alt='Github'
+                    className='h-6 mr-3'
+                  />
+                  Tiếp tục với Github
+                </button>
+
+                <p className='text-xs text-black-400'>
+                  Bằng việc tiếp tục, bạn đồng ý với{' '}
+                  <span className='font-bold'>Điều Khoản Sử Dụng</span>
+                  và <span className='font-bold'>Chính Sách Bảo Mật.</span>
+                </p>
               </div>
             )}
             {activeTab == '2' && <div className='p-8'>Hello2</div>}
+          </div>
+        </div>
+
+        <div className=' flex flex-col justify-center py-20 px-1'>
+          <p className='text-lg text-black-400 mb-3.5'>Chào mừng bạn đến với</p>
+          <img src='https://accounts.topdev.vn/asset/images/topdev-banner.svg'></img>
+          <div className='px-2 py-1 flex justify-center'>
+            <div
+              className='px-3.5 py-4 flex flex-col items-center rounded'
+              style={{
+                maxWidth: '600px',
+                boxShadow: '0px 0px 4px 0px rgba(134, 134, 134, 0.2509803922)',
+              }}
+            >
+              <img
+                src='https://accounts.topdev.vn/asset/images/img-person.svg'
+                alt='Topdev person'
+                width='291'
+                height='243'
+              />
+              <div>
+                <h2 className='text-sm text-black-600 font-semibold mt-2.5 mb-2'>
+                  Đăng nhập ngay để tận dụng tối đa các công cụ của TopDev và gia tăng cơ hội tiếp
+                  cận công việc IT hot nhất
+                </h2>
+                <ul className='list-disc mb-3.5 ml-7'>
+                  <li>Tạo CV chuẩn Developer</li>
+                  <li>Ứng tuyển nhanh chóng hơn với hồ sơ đã được lưu</li>
+                  <li>Quản lý hồ sơ ứng tuyển và theo dõi cập nhật trạng thái ứng tuyển</li>
+                  <li>Xem được mức lương cho mỗi vị trí</li>
+                  <li>Lưu lại công việc yêu thích để ứng tuyển sau</li>
+                  <li>Thực hiện bài trắc nghiệm đánh giá tính cách làm việc</li>
+                </ul>
+                <p className='text-xs mb-3.5 text-black-400'>
+                  Nếu bạn gặp khó khăn trong việc đăng nhập / tạo tài khoản, vui lòng liên hệ với
+                  TopDev qua email contact@topdev.vn
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
