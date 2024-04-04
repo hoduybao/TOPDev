6c5fdc6bdf820983419446bed064d429953ff594
trungnguyen <hotrungnguyen168@gmail.com>
2024-04-04 12:35:38 +0700

    Fetch one job by id

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://demographql.onrender.com/',
+  uri: 'https://demographql.onrender.com/graphql',
   cache: new InMemoryCache(),
 });
 
--- src/pages/recruitment/MainPage.tsx
+++ src/pages/recruitment/MainPage.tsx
@@ -7,16 +7,41 @@ import ApplicationCard from '../../components/global/Recruitment/Content/Applica
 
 import { JobType } from '@/+core/utilities/types/recruitment.type';
 
-import JobMockData from '../../draft/job.json';
 import { gql, useLazyQuery } from '@apollo/client';
 
 type JobsQueryResult = {
   jobs: JobType[];
 };
 
+type JobQueryResult = {
+  job: JobType;
+};
+
 const GET_JOBS = gql`
   query Jobs {
     jobs {
+      id
+      companyId
+      title
+      salary
+      responsibilities
+      skills
+      extends
+      welfare
+      experienceYearsMin
+      experienceYearsMax
+      level
+      type
+      typeContract
+      techs
+      interviewProcess
+    }
+  }
+`;
+
+const GET_JOB_BY_ID = gql`
+  query Job($id: ID!) {
+    job(id: $id) {
       id
       companyId
       title
@@ -43,14 +68,35 @@ const MainPage = () => {
   const [jobsPerPage, setJobsPerPage] = useState<JobType[]>([]);
   const [activePage, setActivePage] = useState<number>(1);
 
-  const [fetchData, { loading, error, data }] = useLazyQuery<JobsQueryResult>(GET_JOBS);
+  const [jobDetail, setJobDetail] = useState<JobType>();
+  const [inputValue, setInputValue] = useState('');
+
+  const [notFound, setNotFound] = useState<boolean>(false);
+
+  const [fetchAllJobs, { loading: fetchAllLoading, error: fetchAllError, data: fetchAllData }] =
+    useLazyQuery<JobsQueryResult>(GET_JOBS);
+  const [fetchOneJob, { loading: fetchOneLoading, error: fetchOneError, data: fetchOneData }] =
+    useLazyQuery<JobQueryResult>(GET_JOB_BY_ID);
 
   // Use for mock jobs data from graphql API
   useEffect(() => {
-    if (data) {
-      setJobs(data?.jobs);
+    if (fetchAllData) {
+      setJobs(fetchAllData?.jobs);
     }
-  }, [data]);
+  }, [fetchAllData]);
+
+  // Use for mock job data from graphql API
+  useEffect(() => {
+    setJobDetail(undefined);
+    setNotFound(false);
+    if (fetchOneData) {
+      if (fetchOneData.job) {
+        setJobDetail(fetchOneData.job);
+      } else {
+        setNotFound(true);
+      }
+    }
+  }, [fetchOneData]);
 
   // Pagination handle
   const handleGetJobsPerPage = (n: number) => {
@@ -63,8 +109,20 @@ const MainPage = () => {
     }
   };
 
-  const handleClick = () => {
-    fetchData();
+  const handleClickFetchAll = () => {
+    fetchAllJobs();
+  };
+
+  const handleClickFetchOne = () => {
+    fetchOneJob({
+      variables: {
+        id: inputValue, // Thay thế "YOUR_JOB_ID_HERE" bằng ID thực tế của công việc bạn muốn lấy thông tin
+      },
+    });
+  };
+
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
   };
 
   useEffect(() => {
@@ -75,33 +133,88 @@ const MainPage = () => {
   return (
     <div className='flex flex-col'>
       <SubHeader jobs={jobs} setJobs={setJobs} />
-      <div>
-        <Button onClick={handleClick}>Fetch data</Button>
+
+      <div className='flex gap-8'>
+        <div>
+          <Button onClick={handleClickFetchAll}>Fetch All</Button>
+        </div>
+
+        <div>
+          <input type='text' value={inputValue} onChange={handleInputChange}></input>
+          <Button onClick={handleClickFetchOne}>Fetch One</Button>
+        </div>
       </div>
 
-      {loading && <p>Loading...</p>}
-      {error && <p>Error : {error.message}</p>}
-      {data && (
-        <>
-          <div className='px-4 py-2.5 flex gap-5 flex-wrap'>
-            {jobsPerPage?.map((job: JobType) => {
-              return (
-                <ApplicationCard key={uuidv4()} job={job} newestAmount={3} recentAmount={150} />
-              );
-            })}
-          </div>
-          <div className='my-5 flex items-center justify-center'>
-            <Pagination
-              current={activePage}
-              total={jobs?.length}
-              pageSize={ITEMS_PER_PAGE} // items per page
-              onChange={(page: number) => {
-                setActivePage(page);
-              }}
-            />
-          </div>
-        </>
+      {fetchAllLoading && <p>Loading...</p>}
+      {fetchAllError && <p>Error : {fetchAllError.message}</p>}
+      <div className='px-4 py-2.5 flex gap-5 flex-wrap'>
+        {jobsPerPage?.map((job: JobType) => {
+          return <ApplicationCard key={uuidv4()} job={job} newestAmount={3} recentAmount={150} />;
+        })}
+      </div>
+
+      {jobDetail && (
+        <div>
+          <h1>{jobDetail.title}</h1>
+          <p>
+            <strong>Company:</strong> {jobDetail.companyId}
+          </p>
+          <p>
+            <strong>Salary:</strong> {jobDetail.salary}
+          </p>
+          <p>
+            <strong>Responsibilities:</strong> {jobDetail.responsibilities?.join(', ')}
+          </p>
+          <p>
+            <strong>Skills:</strong> {jobDetail.skills?.join(', ')}
+          </p>
+          <p>
+            <strong>Extends:</strong> {jobDetail.extends?.join(', ')}
+          </p>
+          <p>
+            <strong>Welfare:</strong> {jobDetail.welfare?.join(', ')}
+          </p>
+          <p>
+            <strong>Min Experience Years:</strong> {jobDetail.experienceYearsMin}
+          </p>
+          <p>
+            <strong>Max Experience Years:</strong> {jobDetail.experienceYearsMax}
+          </p>
+          <p>
+            <strong>Level:</strong> {jobDetail.level}
+          </p>
+          <p>
+            <strong>Type:</strong> {jobDetail.type}
+          </p>
+          <p>
+            <strong>Type Contract:</strong> {jobDetail.typeContract}
+          </p>
+          <p>
+            <strong>Techs:</strong> {jobDetail.techs?.join(', ')}
+          </p>
+          <p>
+            <strong>Interview Process:</strong> {jobDetail.interviewProcess?.join(', ')}
+          </p>
+          <p>
+            <strong>Description:</strong> {jobDetail.description}
+          </p>
+        </div>
       )}
+
+      {fetchOneLoading && <p>Loading...</p>}
+      {fetchOneError && <p>Error : {fetchOneError.message}</p>}
+      {notFound && <p>404: Not Found</p>}
+
+      <div className='my-5 flex items-center justify-center'>
+        <Pagination
+          current={activePage}
+          total={jobs?.length}
+          pageSize={ITEMS_PER_PAGE} // items per page
+          onChange={(page: number) => {
+            setActivePage(page);
+          }}
+        />
+      </div>
     </div>
   );
 };
