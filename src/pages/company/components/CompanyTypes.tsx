5c77f20889a6c82a185073a433168d06509494de
qhungo0125 <hungngojan25@gmail.com>
2024-04-10 16:09:05 +0700

    add company types select

--- src/pages/company/components/CompanyTypes.tsx
+++ src/pages/company/components/CompanyTypes.tsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+type Company = {
+  alias: string;
+  url: string;
+  isSelected?: boolean;
+};
+const CompanyItem = ({ alias, url, isSelected = false }: Company) => {
+  const { t } = useTranslation();
+  return (
+    <div
+      className={`
+      py-2 border-[0.5px] border-orange-500 text-center
+      ${isSelected ? 'text-white-900 bg-orange-500' : ' text-black-900 bg-white-900'} `}
+    >
+      <Link className='capitalize font-bold text-base' to={url}>
+        {t(alias)}
+      </Link>
+    </div>
+  );
+};
 
 const CompanyTypes = () => {
-  return <div>CompanyTypes</div>;
+  const list: Company[] = [
+    { alias: 'trend.top', url: '/', isSelected: true },
+    { alias: 'trend.follow', url: '/' },
+    { alias: 'trend.job', url: '/' },
+    { alias: 'trend.total', url: '/' },
+  ];
+  return (
+    <div className='flex'>
+      {list.map((item, index) => (
+        <div className='w-[25%]'>
+          <CompanyItem key={index} {...item} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default CompanyTypes;
