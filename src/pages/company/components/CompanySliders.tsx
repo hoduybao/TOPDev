8bfe7928053132015c2526430d151c6c770c4822
qhungo0125 <hungngojan25@gmail.com>
2024-04-10 18:36:34 +0700

    add pagination for slider

--- src/pages/company/components/CompanySliders.tsx
+++ src/pages/company/components/CompanySliders.tsx
@@ -4,9 +4,9 @@ import companyData from '../../../draft/company-new.json';
 import 'swiper/css';
 import { useTranslation } from 'react-i18next';
 import { ICONS } from '@/config/icons';
-import { Link } from 'react-router-dom';
-import { ArrowRightOutlined } from '@ant-design/icons';
-import { Autoplay } from 'swiper/modules';
+import { Link, useNavigate } from 'react-router-dom';
+import { ArrowRightOutlined, CaretUpOutlined } from '@ant-design/icons';
+import { Autoplay, Navigation, Pagination } from 'swiper/modules';
 type Props = {
   type: string;
   size: number;
@@ -29,8 +29,14 @@ export const SliderItem = ({
   company: SliderCompanyItem;
   extraClass?: string;
 }) => {
+  const navigate = useNavigate();
   return (
-    <div className={'w-full h-full border-[1px] border-gray-300 ' + extraClass}>
+    <div
+      onClick={() => {
+        navigate('/companies/company1');
+      }}
+      className={'w-full h-full border-[1px] border-gray-300 overflow-visible ' + extraClass}
+    >
       <div className='mb-2'>
         <img
           src={company.background}
@@ -39,11 +45,20 @@ export const SliderItem = ({
           className='w-full h-[200px] object-cover'
         />
         <div className='w-full h-[3rem] px-4 flex items-end gap-2'>
-          <img
-            src={company.avatar}
-            alt={company.avatar}
-            className='w-[6rem] h-[77px] object-fill shadow-md'
-          />
+          <div className='h-[77px] relative group'>
+            <img
+              src={company.avatar}
+              alt={company.avatar}
+              className='w-[6rem] h-full object-fill shadow-md'
+            />
+            <div className='hidden group-hover:block absolute w-[12rem] translate-x-[-50%] left-[50%] z-10 rounded p-2 text-center'>
+              <div className='relative flex justify-center rounded bg-gray-400 p-2'>
+                <CaretUpOutlined className='absolute top-[-0.6rem] text-gray-400' />
+                <span className=' text-white-900'>{company.name + ' ' + company.slogan}</span>
+              </div>
+            </div>
+          </div>
+
           <h4 className='flex-1'>{company.name}</h4>
         </div>
       </div>
@@ -62,7 +77,7 @@ export const SliderItem = ({
         <div className='col-span-12 text-gray-400 flex justify-between'>
           <div className='flex-1 truncate max-w-[70%]'>{company.fields.join(',')}</div>
 
-          <Link to='/'>
+          <Link to='/companies/comany1'>
             <div className='flex gap-1 font-bold'>
               <span className='text-orange-500 underline'>3 Jobs</span>
               <ArrowRightOutlined className='text-orange-500' />
@@ -88,13 +103,14 @@ const Sliders = ({ size, autoplay }: { size: number; autoplay: boolean }) => {
     slidesPerView: size,
     autoHeight: true,
     loop: true,
-    modules: autoplay ? [Autoplay] : [],
+    modules: autoplay ? [Autoplay, Navigation, Pagination] : [Navigation, Pagination],
     autoplay: autoplay ? { delay: 3000 } : {},
+    scrollbar: { draggable: true },
   };
 
   return (
     <div>
-      <Swiper {...swiperProps}>
+      <Swiper {...swiperProps} navigation={true}>
         {data.map((company: SliderCompanyItem, index: number) => (
           <SwiperSlide key={company.id + index}>
             <SliderItem company={company} />
