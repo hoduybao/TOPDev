c7a17384f216c8328bc48c597d7884b6d878c8a0
qhungo0125 <hungngojan25@gmail.com>
2024-04-19 16:04:13 +0700

    add summary

--- src/pages/UserProfile/components/SummarySession.tsx
+++ src/pages/UserProfile/components/SummarySession.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import SummaryModal from './SummaryModal';
 
 const SummarySession = () => {
-  return <div>SummarySession</div>;
+  return (
+    <div className='flex gap-4 justify-around rounded bg-white-900 p-4'>
+      <div className='flex-1'>
+        <h3 className='text-2xl font-bold text-black-900'>Summary</h3>
+        <p className='text-gray-400 text-base'>
+          Provide detail information helps us find easily the jobs that fit for you.
+        </p>
+      </div>
+      <div className='px-4'>
+        <img src='/assets/icons/summary.svg' alt='summary' />
+      </div>
+      <div>
+        <SummaryModal />
+      </div>
+    </div>
+  );
 };
 
 export default SummarySession;
