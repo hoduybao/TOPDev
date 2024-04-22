0364868467616d274fda443f69aa2d4f838840c5
qhungo0125 <hungngojan25@gmail.com>
2024-04-22 12:47:53 +0700

    clean route layout

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -26,74 +26,50 @@ import ManageFollowPage from '@/pages/manage-follow/ManageFollowPage';
 
 export const router = createBrowserRouter([
   {
-    path: '/it-jobs',
-    element: <UserLayout />,
-    children: [{ path: MY_ROUTE.IT_JOBS, element: <ITJobs /> }],
-  },
-  {
     path: '/recruitment',
     element: <RecruitmentLayout />,
-    children: [{ path: MY_ROUTE.RECRUITMENT, element: <MainPage /> }],
+    children: [
+      { path: MY_ROUTE.RECRUITMENT, element: <MainPage /> },
+      { path: MY_ROUTE.RECRUITMENT_DETAIL_JOB, element: <DetailJobPage /> },
+    ],
   },
   {
     path: '/recruitment/:jobId',
-    element: <RecruitmentLayout />,
-    children: [{ path: MY_ROUTE.RECRUITMENT_DETAIL_JOB, element: <DetailJobPage /> }],
-  },
-  {
-    path: '/recruitment/:jobId/applications',
     element: <RecruitmentProcessLayout />,
-    children: [{ path: MY_ROUTE.RECRUITMENT_PROCESS, element: <ProcessPage /> }],
-  },
-  {
-    path: '/recruitment/:jobId/application/:applicationId',
-    element: <RecruitmentProcessLayout />,
-    children: [{ path: MY_ROUTE.RECRUITMENT_DETAIL, element: <DetailPage /> }],
+    children: [
+      { path: MY_ROUTE.RECRUITMENT_PROCESS, element: <ProcessPage /> },
+      { path: MY_ROUTE.RECRUITMENT_DETAIL, element: <DetailPage /> },
+    ],
   },
   {
     path: '/admin',
     element: <AdminLayout />,
-    children: [{ path: MY_ROUTE.ADMIN, element: <AdminMainPage /> }],
+    children: [
+      { path: MY_ROUTE.ADMIN, element: <AdminMainPage /> },
+      { path: MY_ROUTE.ADMIN_LOGIN, element: <AdminLoginPage /> },
+      { path: MY_ROUTE.ADMIN_ACCOUNT_MANAGEMENT, element: <AccountManagementPage /> },
+    ],
   },
   {
-    path: MY_ROUTE.ADMIN_LOGIN,
-    element: <AdminLayout />,
-    children: [{ path: MY_ROUTE.ADMIN_LOGIN, element: <AdminLoginPage /> }],
-  },
-  {
-    path: MY_ROUTE.ADMIN_ACCOUNT_MANAGEMENT,
-    element: <AdminLayout />,
-    children: [{ path: MY_ROUTE.ADMIN_ACCOUNT_MANAGEMENT, element: <AccountManagementPage /> }],
-  },
-  {
-    path: '/login',
+    path: '/',
     element: <LoginLayout />,
-    children: [{ path: MY_ROUTE.LOGIN, element: <LoginPage /> }],
-  },
-  {
-    path: '/reset-password',
-    element: <LoginLayout />,
-    children: [{ path: MY_ROUTE.RESET_PASSWORD, element: <ResetPasswordPage /> }],
-  },
-  {
-    path: MY_ROUTE.JOB_MANAGEMENT,
-    element: <UserLayout />,
-    children: [{ path: MY_ROUTE.JOB_MANAGEMENT, element: <JobManagement /> }],
-  },
-  {
-    path: MY_ROUTE.USER_PROFILE,
-    element: <UserLayout />,
-    children: [{ path: MY_ROUTE.USER_PROFILE, element: <MyCV /> }],
+    children: [
+      { path: MY_ROUTE.LOGIN, element: <LoginPage /> },
+      { path: MY_ROUTE.RESET_PASSWORD, element: <ResetPasswordPage /> },
+    ],
   },
   {
     path: '/',
     element: <UserLayout />,
     children: [
+      { path: MY_ROUTE.USER_PROFILE, element: <MyCV /> },
+      { path: MY_ROUTE.JOB_MANAGEMENT, element: <JobManagement /> },
       { path: MY_ROUTE.HOME, element: <HomePage /> },
       { path: MY_ROUTE.JOB, element: <JobPage /> },
       { path: MY_ROUTE.COMPANIES, element: <CompaniesPage /> },
       { path: MY_ROUTE.COMPANY, element: <CompanyPage /> },
-      { path: '/manage-follow', element: <ManageFollowPage /> },
+      { path: MY_ROUTE.MANAGE_FOLLOW, element: <ManageFollowPage /> },
+      { path: MY_ROUTE.IT_JOBS, element: <ITJobs /> },
     ],
   },
 
