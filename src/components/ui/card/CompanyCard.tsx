b41ed096f67b5e4bdd053516cbe45c78df8b7755
qhungo0125 <hungngojan25@gmail.com>
2024-04-28 03:22:45 +0700

    fix css

--- src/components/ui/card/CompanyCard.tsx
+++ src/components/ui/card/CompanyCard.tsx
@@ -7,22 +7,26 @@ const CompanyCard = ({ isSticky }: { isSticky: boolean }) => {
   const { data: jobResponse, isLoading: isLoadingJob } = useGetJobByIdQuery(jobId);
 
   return (
-    <Spin spinning={isLoadingJob}>
-      <div className={`mr-4 p-2 w-[16.67%] ${isSticky ? 'hidden' : ''}`}>
-        <img className='min-w-[70px]' src={jobResponse?.data?.company?.image} alt='' />
-      </div>
+    <>
+      {jobResponse ? (
+        <>
+          <div className={`mr-4 p-2 w-[16.67%] ${isSticky ? 'hidden' : ''}`}>
+            <img className='min-w-[70px]' src={jobResponse?.data?.company?.image} alt='' />
+          </div>
 
-      <div className={` ${isSticky ? 'w-[100%]' : 'w-[83.33%]'}`}>
-        <div className='font-bold text-2xl'>{jobResponse && jobResponse.data.title}</div>
-        <div className='uppercase opacity-70 font-bold text-lg'>
-          {jobResponse?.data?.company?.name}
-        </div>
-        <div className={`mt-2 text-base opacity-70`}>{jobResponse?.data?.company?.address}</div>
-        <div className={`mt-2 text-base text-orange-600 font-semibold`}>
-          {jobResponse?.data?.salary} $
-        </div>
-      </div>
-    </Spin>
+          <div className={` ${isSticky ? 'w-[100%]' : 'w-[83.33%]'}`}>
+            <div className='font-bold text-2xl'>{jobResponse && jobResponse.data.title}</div>
+            <div className='uppercase opacity-70 font-bold text-lg'>
+              {jobResponse?.data?.company?.name}
+            </div>
+            <div className={`mt-2 text-base opacity-70`}>{jobResponse?.data?.company?.address}</div>
+            <div className={`mt-2 text-base text-orange-600 font-semibold`}>
+              {jobResponse?.data?.salary} $
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
   );
 };
 
