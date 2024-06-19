114d9360a8fa03c1243860aaa4fe776471994d76
20120433 <82367666+hoduybao@users.noreply.github.com>
2024-06-19 21:35:32 +0700

    fix https

--- src/+core/redux/apis/common.api.ts
+++ src/+core/redux/apis/common.api.ts
@@ -5,7 +5,7 @@ export const commonApi = createApi({
   reducerPath: 'CommonApi',
   keepUnusedDataFor: 1,
   baseQuery: baseQueryWithAuth({
-    baseUrl: `${import.meta.env.VITE_BACKEND}`,
+    baseUrl: `http://orichalcos.me`,
     // baseUrl: `http://20.191.157.139`,
     async prepareHeaders(headers) {
       // if have token, set Authorization header
