536770cc34d9f1d48a9ef0ced612fac1d95ec818
20120433 <82367666+hoduybao@users.noreply.github.com>
2024-06-21 00:50:17 +0700

    fix

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,28 @@
+import fetch from 'node-fetch';
+
+export async function handler(event, context) {
+  try {
+    const data = JSON.parse(event.body);
+    const { pageURL } = data;
+
+    const res = await fetch(pageURL);
+    const htmlContent = await res.text();
+
+    return {
+      statusCode: 200,
+      body: htmlContent,
+    };
+  } catch (e) {
+    let responseBody = 'Something bad happened!';
+    if (e instanceof SyntaxError) {
+      responseBody = 'Bad JSON!';
+    } else if (e instanceof TypeError) {
+      responseBody = 'Bad URL!';
+    }
+
+    return {
+      statusCode: 404,
+      body: responseBody,
+    };
+  }
+}
